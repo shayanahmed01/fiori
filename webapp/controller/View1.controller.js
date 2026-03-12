c78330d2ab34b7b197880625f9136820bab26562
Moeez Bin Nadeem <moeez1280@gmail.com>
2026-03-12 12:47:00 +0500

    INITIAL fiori setup

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("zfi.demo.project1.controller.View1", {
+        onInit() {
+        }
+    });
+});
